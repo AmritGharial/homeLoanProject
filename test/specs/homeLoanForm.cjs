98641daabb27b1ab884a9d66f2d4a441a60b07bb
Parteek Goel <prateekgoel@bugraptors.com>
2025-03-25 18:22:09 +0530

    updates

--- test/specs/homeLoanForm.cjs
+++ test/specs/homeLoanForm.cjs
@@ -1,18 +1,17 @@
-// const HomeLoanFormPage = require('../pageobjects/homeLoanForm.page.cjs');
-// const Constants = require('../constants/constants.cjs');
+const HomeLoanFormPage = require('../pageobjects/homeLoanForm.page.cjs');
+const Constants = require('../constants/constants.cjs');
  
-// describe('Home Loan Application Form Tests', () => {
-//     beforeEach(async () => {
-//         await HomeLoanFormPage.open();
-//         await browser.maximizeWindow();
-//     });
+describe('Home Loan Application Form Tests', () => {
+    beforeEach(async () => {
+        await HomeLoanFormPage.open();
+        await browser.maximizeWindow();
+    });
  
-//     it('should navigate to OTP page once all form details are correctly filled', async () => {
-//         await HomeLoanFormPage.generateOTPButton.waitForDisplayed({ timeout: 40000 });
-//         await HomeLoanFormPage.fillForm(Constants.fullName, Constants.mobNo, Constants.homeLoanDropdown, Constants.montlyIncome, Constants.pincode, Constants.loanAmount);
-//         await expect(HomeLoanFormPage.generateOtpImage).toBeDisplayed();
-//      });
- 
-// });
+    it('should navigate to OTP page once all form details are correctly filled', async () => {
+        await HomeLoanFormPage.generateOTPButton.waitForDisplayed({ timeout: 40000 });
+        await HomeLoanFormPage.fillForm(Constants.fullName, Constants.mobNo, Constants.homeLoanDropdown, Constants.montlyIncome, Constants.pincode, Constants.loanAmount);
+        await expect(HomeLoanFormPage.generateOtpImage).toBeDisplayed();
+     });
+});
  
  
